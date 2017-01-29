3530d0de9e3848038a1c1caaa319f69fd04a691d
Billie99 <billiearmstrong1886@gmail.com>
2017-01-29 15:25:41 +0100

    Aggiunto nuovo file

---
+++ Input.py
@@ -0,0 +1,7 @@
+# Resrtituisce una stringa
+name = raw_input("Come ti chiami ? ")
+print "Ciao", name
+
+# Restituisce un intero
+numero = input("Inserisci un valore ")
+print "Il numero e'", numero
