5fa613d637733d4c47aa3d4d35765c8e93707054
Billie99 <billiearmstrong1886@gmail.com>
2017-01-31 19:49:54 +0100

    Update Sfera.py

--- Sfera.py
+++ Sfera.py
@@ -1,15 +1,14 @@
 import math
 
 Raggio = input("Inserisci il valore del raggio ")
-pi = math.pi
 
 # Superfice della sfera
 
-Superfice = (4 * pi) * (Raggio * Raggio)
+Superfice = (4 * math.pi) * (Raggio * Raggio)
 print "Il valore della superfice della sfera e':", Superfice
 
 #volume della sfera
 
-Volume = (4.0/3.0) * pi * (Raggio * Raggio * Raggio)
+Volume = (4.0/3.0) * math.pi * (Raggio * Raggio * Raggio)
 print "Il volume della sfera e':", Volume
 
