4a2334b70d4b34e6c09d191a45d3596a9881836d
Billie99 <billiearmstrong1886@gmail.com>
2017-05-18 17:46:24 +0200

    Aggiustamenti

--- no_term/ThePySum.py
+++ no_term/ThePySum.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 numero = 0
 num = 0
@@ -6,8 +7,11 @@ num = 0
 file = os.path.isfile("sum.dat")
 
 if not file:
-   print "Impossibile aprire il file sum.dat, e inesistente "
-   exit(0)
+
+   print "Impossibile aprire il file sum.dat, e' inesistente "
+   time.sleep(2)
+   print "Verra creato un nuovo file"
+   time.sleep(2)
 
 else:
     f = open("sum.dat", "r")
@@ -16,21 +20,22 @@ else:
     num = int(line)
     f.close()
 
-    print "Lettura del file sum.dat in corso ..."
+print "Lettura del file sum.dat in corso ..."
+time.sleep(2)
+print "Valore corrente: ", num
+print " "
+time.sleep(2)
 
-    print "Valore corrente: ", num
-    print " "
+numero = input("Inserisci il valore da sommare ")
 
-    numero = input("Inserisci il valore da sommare ")
+numero = numero + num
+print "Nuovo valore:", numero
+time.sleep(2)	
 
-    numero = numero + num
+f = open("sum.dat", "w")
+f.write(str(numero))
 
-    print "Nuovo valore:", numero
+print "Salvataggio nel file sum.dat in corso ... "
+print " "
 
-    f = open("sum.dat", "w")
-    f.write(str(numero))
-
-    print "Salvataggio nel file sum.dat in corso ... "
-    print " "
-
-    f.close()
+f.close()
