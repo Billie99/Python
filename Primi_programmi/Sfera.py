08b8deab714b4aad8341c76b9ed5a406ed050ebd
Billie99 <giandomenicoiameo99@gmail.com>
2018-04-15 15:57:14 +0200

    Update Sfera.py

--- Primi_programmi/Sfera.py
+++ Primi_programmi/Sfera.py
@@ -2,7 +2,7 @@ import math
 
 raggio = input("Inserisci il valore del raggio ")
 
-# Superfice della sfera
+# Superficie della sfera
 
 superficie = (4.0 * math.pi) * (raggio * raggio)
 print "Il valore della superfice della sfera e':", superficie
