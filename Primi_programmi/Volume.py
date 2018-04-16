93e22b82e67404725cd93054e4c9eb439693488d
Billie99 <giandomenicoiameo99@gmail.com>
2018-04-17 01:51:26 +0200

    mdificato file Volume.py

--- Primi_programmi/Volume.py
+++ Primi_programmi/Volume.py
@@ -2,24 +2,23 @@
 import math
 
 print "Calcolo del volume di un cubo o di una sfera"
-Scelta = input("Effettua una scelta: (1 - Cubo, 2 - Sfera) ")
+scelta = input("Effettua una scelta: (1 - Cubo, 2 - Sfera) ")
 
 if Scelta == 1:
 # Calcolo del volume di un cubo
    
-   Lato = input("Inserisci il valore di un lato del cubo: ")
-   Volume = Lato * Lato * Lato
+   lato = input("Inserisci il valore di un lato del cubo: ")
+   volume = lato * lato * lato
 
-   print "Il volume del cubo e' :", Volume
+   print "Il volume del cubo e' :", volume
 
-elif Scelta == 2:
+elif scelta == 2:
 # Calcolo del volume di una sfera
 
-     Raggio = input("Inserisci il valore del raggio di una sfera ")
-     Volume = (4.0/3.0) * math.pi * (Raggio * Raggio * Raggio)
+     raggio = input("Inserisci il valore del raggio di una sfera ")
+     volume = (4.0/3.0) * math.pi * (raggio * raggio * raggio)
 
-     print "Il volume della sfera e':", Volume
+     print "Il volume della sfera e':", volume
  
 else:
      print "Il numero che hai inserito non esiste"
-
